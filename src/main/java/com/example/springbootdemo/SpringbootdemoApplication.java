8a63c58cd299278901cbbb6d0d0cb0b03950d292
Jeremy <jeremyeparker@yahoo.com>
2021-04-03 21:50:20 -0400

    return a Hello World to localhost

--- src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -2,12 +2,21 @@ package com.example.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+//makes class a REST-ful endpoint
+@RestController
 public class SpringbootdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello(){
+		return "Hello Wrold";
 	}
 
 }
