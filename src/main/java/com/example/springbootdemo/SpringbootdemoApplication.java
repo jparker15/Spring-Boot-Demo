a602186d2ef961614e86ce6a01485cad8aea0744
Jeremy <jeremyeparker@yahoo.com>
2021-04-04 23:34:58 -0400

    get List of Student classes, enter mock student

--- src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.springbootdemo;
 
+import com.example.springbootdemo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 //makes class a REST-ful endpoint
@@ -15,8 +20,10 @@ public class SpringbootdemoApplication {
 	}
 
 	@GetMapping
-	public String hello(){
-		return "Hello Wrold";
+	public List<Student> hello(){
+		return List.of(
+				new Student(1L,"Jermy","jar@gmail.com", LocalDate.of(1995, Month.DECEMBER, 22), 25)
+		);
 	}
 
 }
